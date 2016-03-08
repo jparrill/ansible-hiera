4bde6ffd552b9e97d3417098c6241d1957e4fc9c
Juan Manuel Parrilla <padajuan@gmail.com>
2016-03-08 01:39:51 +0100

    license change

--- library/hiera.py
+++ library/hiera.py
@@ -3,11 +3,18 @@
 """
 Hiera-Ansible Parser.
 
-Copyright (c) 2016
-Juan Manuel Parrilla <jparrill@redhat.com>
-
-This software may be freely redistributed under the terms of the MIT License
-more details into LICENSE file
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
@@ -16,6 +23,7 @@ DOCUMENTATION = '''
 module: hiera
 version_added: "1.0.0"
 short_description: Use Hiera key/values into Ansible as fact.
+author: "Jua"
 description:
     - Once copied all Hiera Data from PuppetMaster and Hiera.yaml into
     destination node, this module will parse all key-value that you need
